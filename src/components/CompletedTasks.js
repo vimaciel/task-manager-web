7e19080f4d872b4cb50342e31f6514ccd1d86841
vimaciel <viniciusfmaciel@gmail.com>
2019-10-29 22:25:30 -0300

    Get completed tasks from api and shows the result on screen

--- src/components/CompletedTasks.js
+++ src/components/CompletedTasks.js
@@ -1,13 +1,43 @@
 import React, { PureComponent, Fragment } from 'react'
 import Header from './Header'
+import TaskCard from './TaskCard'
+import { connect } from 'react-redux'
+import { getTasks } from '../actions/tasks'
 
-export default class CompletedTasks extends PureComponent {
+class CompletedTasks extends PureComponent {
+    componentDidMount(){
+        this.props.getCompletedTasks()
+    }
+
     render() {
+        const { tasks } = this.props
+
         return (
             <Fragment>
                 <Header title="Completed Tasks"/>
-                <span>Completed Tasks</span>
+                <div className="tasks">
+                    {tasks.map(task => (
+                        <TaskCard key={task.id} task={task}/>                    
+                    ))}
+                </div>
             </Fragment>
         )
     }
 }
+
+const mapStateToProps = ({ tasks }) => {
+    return {
+        tasks
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getCompletedTasks: () => {
+            dispatch(getTasks(true))
+        }
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompletedTasks)
--- src/helpers/taskApi.js
+++ src/helpers/taskApi.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 const url = 'http://localhost:3001/tasks/'
 
 export async function getTasks(onlyCompleted) {
-    const { data } = await axios.get(`${url}?onlyCompleted=${onlyCompleted}`)
+    const { data } = await axios.get(`${url}?completed=${onlyCompleted}`)
     return data
 }
